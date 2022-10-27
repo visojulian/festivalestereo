5721347f9f7e553bf09ba19be0e52275e79f8c0f
Julian Viso <julian.viso.romero@gmail.com>
2022-10-27 17:58:47 -0300

    correcciones varias

--- js/app.js
+++ js/app.js
@@ -7,6 +7,68 @@ window.onload = () => {
   }
   $('.autoplay').slick({
     slidesToShow: 4,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.shows-autoplay').slick({
+    slidesToShow: 6,
+    pauseOnHover: false,
+    swipeToSlide: true,
+    pauseOnFocus: false,
+    slidesToScroll: 1,
+    autoplay: true,
+    autoplaySpeed: 2000,
+    prevArrow: $('.prev'),
+    nextArrow: $('.next'),
+    responsive: [
+      {
+        breakpoint: 768,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      },
+      {
+        breakpoint: 480,
+        settings: {
+          slidesToShow: 1,
+          slidesToScroll: 1
+        }
+      }
+      // You can unslick at a given breakpoint now by adding:
+      // settings: "unslick"
+      // instead of a settings object
+    ]
+  });
+  $('.shows-rtl-autoplay').slick({
+    slidesToShow: 6,
+    rtl: true,
+    swipeToSlide: true,
+    pauseOnHover: false,
+    pauseOnFocus: false,
+    initialSlide: 13,
     slidesToScroll: 1,
     autoplay: true,
     autoplaySpeed: 2000,
